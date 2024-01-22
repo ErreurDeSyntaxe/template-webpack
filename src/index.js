597e554801f378c4078e84fcece42c1f5060166f
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-01-22 14:41:59 +0800

    Fix instructions for self

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Template JavaScript File
 
 // Install webpack with 'npm install webpack webpack-cli --save-dev'
-// Automatically compile code with 'npm run watch', stop with ctrl+c
+// Automatically compile code with 'npm run watch', stop with CTRL + C
 
 import './style.css';
 import Logo from './img/odin-logo.svg';
