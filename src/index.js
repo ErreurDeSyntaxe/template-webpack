03f505e632c1f515c12bfb824c96c4c26f8857be
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-01-22 14:12:55 +0800

    Setup Webpack, step 3

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
 // The command for webpack to recompile code on save is 'npm run watch'
 // Just write that in the CLI and let webpack 'watch' for changes
 
-// import './style.css';
-// import './img/odin-logo.svg';
+import './style.css';
+import Logo from './img/odin-logo.svg';
 import greetMe from './script';
 
-// const odinLogo = new Image();
-// odinLogo.src = Logo;
-// const body = document.querySelector('body');
-// body.appendChild(odinLogo);
+const odinLogo = new Image();
+odinLogo.src = Logo;
+const body = document.querySelector('body');
+body.appendChild(odinLogo);
 
 console.log('Hello, World!');
 greetMe();
